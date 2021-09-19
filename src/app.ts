71877715d5cfd8670e509f8774dd6e2d5fc4d783
unifino <unifino@protonmail.com>
2021-09-19 17:54:41 +0300

    my app

--- src/app.ts
+++ src/app.ts
@@ -11,10 +11,10 @@ const app = express();
 
 // -- ======================================================================== SETUP =======
 
-app.use( bodyParser.urlencoded( { extended: false } ) );
+app.use( bodyParser.json( { limit: '50mb' } ) );
+app.use( bodyParser.urlencoded( { limit: '50mb', extended: true } ) );
+app.use( express.json() );
 app.use( bodyParser.json() );
-app.use( express.json( { limit: '50mb' } ) );
-app.use( express.urlencoded ({ limit: '50mb', extended: true, parameterLimit: 50000 } ) );
 
 // -- ============================================================ List of BeautyBGs =======
 
