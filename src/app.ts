e4123b23aae933c42ba75678e8ca35ffa618b14d
unifino <unifino@protonmail.com>
2021-09-21 07:43:29 +0300

    ram.

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use( bodyParser.json( { limit: '50mb' } ) );
 app.use( bodyParser.urlencoded( {
     limit: '50mb',
     extended: true,
-    parameterLimit: 50000 
+    parameterLimit: 50000
 } ) );
 
 // -- ============================================================ List of BeautyBGs =======
