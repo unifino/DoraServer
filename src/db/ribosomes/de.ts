c646e3f0631cd80b366e92f81bd6d475470dc0a8
unifino <unifino@protonmail.com>
2021-09-23 18:08:33 +0300

    my app

--- src/db/ribosomes/de.ts
+++ src/db/ribosomes/de.ts
@@ -89,6 +89,17 @@ export const de_r: g.Ribosome[] = [
         source      : "Langenscheidt",
         contains    : null,
         readMode    : "start"
+    },
+    {
+        institute   : "de",
+        code        : "DWMKPLZ",
+        type        : "audio",
+        level       : "B2",
+        title       : "Marktplatz",
+        avatar      : "https://cdn.pixabay.com/photo/2016/12/21/16/34/shopping-cart-1923313_960_720.pngz",
+        source      : "dw.com",
+        contains    : null,
+        readMode    : "start"
     }
 
 ]
