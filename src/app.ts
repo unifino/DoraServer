2e11305343faeb1a2426948ee8da85d74d017d6d
unifino <unifino@protonmail.com>
2021-12-22 13:43:30 +0300

    my app

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import * as u                           from './types/user';
 // -- ========================================================================= TEST =======
 
 // .. https://stark-chamber-36060.herokuapp.com/
-import { get }                          from 'https';
+import { request }                          from 'https';
 
 // -- ========================================================================= INIT =======
 
@@ -36,7 +36,7 @@ app.get( '/test', ( req: express.Request, res: express.Response ) => {
         url: link,
         headers: { "User-Agent": userAgent }
     };
-    get( options, res_2 => {
+    request( options, res_2 => {
 
         let html = "";
 
