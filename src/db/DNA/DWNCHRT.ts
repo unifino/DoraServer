2b6011dacb758ead868b3bbead1045d497af799e
unifino <unifino@protonmail.com>
2024-03-25 16:16:21 +0330

    ...

--- src/db/DNA/DWNCHRT.ts
+++ src/db/DNA/DWNCHRT.ts
@@ -13,7 +13,7 @@ export function DNA_maker (): Promise<g.gene[]> {
     return new Promise ( (rs, rx) => {
 
         let heute = heuteIst();
-        let homeURL = "learngerman.dw.com/de/langsam-gesprochene-nachrichten/s-60040332";
+        let homeURL = "http://learngerman.dw.com/de/langsam-gesprochene-nachrichten/s-60040332";
 
         try {
             _( homeURL ).then( homePage => {
